9f9c628d3e88d814eac82f714aa7c21afb4015c9
mert-solak <solak.mert@hotmail.com>
2021-05-16 00:41:28 +0300

    feat(src/pages/*): basic tasks page created

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -1,2 +1,5 @@
 export * as eventDefinitions from './event.definition';
 export * as userDefinitions from './user.definitions';
+
+export * as reduxUserDefinitions from './redux-user.definition';
+export * as reduxRootDefinitions from './redux-root.definition';
---
+++ src/pages/Tasks.page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TasksPage = () => <p>Tasks</p>;
+
+export default TasksPage;
