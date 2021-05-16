4dd3e4d0f24818ae2cf573d8f777f8170eb04c9a
mert-solak <solak.mert@hotmail.com>
2021-05-16 03:20:46 +0300

    feat(src/configs/*): axios and url config update

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,4 @@
 export * as envConfig from './env.config';
 export * as eventConfig from './event.config';
 export * as urlConfig from './url.config';
+export * as axiosConfig from './axios.config';
--- src/configs/url.config.ts
+++ src/configs/url.config.ts
@@ -3,3 +3,7 @@ import { envConfig } from '.';
 export const urls = {
   api: envConfig.env.API,
 };
+
+export const routes = {
+  tasks: `${urls.api}/tasks`,
+};
