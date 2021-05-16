5de3985857f6218707f44a8212e97a73af21d468
mert-solak <solak.mert@hotmail.com>
2021-05-16 12:07:26 +0300

    feat(src/configs/axios.config.ts): axios config created

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -1,3 +1,13 @@
+import { snackbarEvents } from '../events';
+import { errorLocale } from '../locales';
+
 export const options = {
   isLoadingBlocked: false,
+  isErrorHandlerBlocked: false,
+  handleErrorsBy: 'name',
+  handleErrorsWith: errorLocale.errorMessages,
+};
+
+export const errorHandler = (errorMessage: string) => {
+  snackbarEvents.sendErrorMessage(errorMessage);
 };
