2e4b1fec23af0a625a93d65f77d471c6cf60a4ea
mert-solak <solak.mert@hotmail.com>
2021-05-16 15:14:51 +0300

    feat(src/*): task service updated

--- src/definitions/service-task.definitions.ts
+++ src/definitions/service-task.definitions.ts
@@ -6,5 +6,13 @@ export interface GetTasksInput {
   limit?: number;
   offset?: number;
 }
-
 export type GetTasksReturn = taskDefinitions.Task[];
+
+export type UpdateTaskInput = Partial<taskDefinitions.Task> & { _id: taskDefinitions.Task['_id'] };
+export type UpdateTaskReturn = taskDefinitions.Task;
+
+export type DeleteTaskInput = { _id: taskDefinitions.Task['_id'] };
+export type DeleteTaskReturn = taskDefinitions.Task;
+
+export type CreateTaskInput = Omit<taskDefinitions.Task, '_id'>;
+export type CreateTaskReturn = taskDefinitions.Task;
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -11,3 +11,33 @@ export const getTasks = (
     url: urlConfig.routes.tasks,
     params,
   });
+
+export const CreateTask = (
+  axios: AxiosInstance,
+  data: serviceTaskDefinitions.CreateTaskInput,
+): AxiosPromise<serviceTaskDefinitions.CreateTaskReturn> =>
+  axios.request({
+    method: 'POST',
+    url: urlConfig.routes.tasks,
+    data,
+  });
+
+export const deleteTask = (
+  axios: AxiosInstance,
+  params: serviceTaskDefinitions.DeleteTaskInput,
+): AxiosPromise<serviceTaskDefinitions.DeleteTaskReturn> =>
+  axios.request({
+    method: 'DELETE',
+    url: urlConfig.routes.tasks,
+    params,
+  });
+
+export const updateTask = (
+  axios: AxiosInstance,
+  data: serviceTaskDefinitions.UpdateTaskInput,
+): AxiosPromise<serviceTaskDefinitions.UpdateTaskReturn> =>
+  axios.request({
+    method: 'PATCH',
+    url: urlConfig.routes.tasks,
+    data,
+  });
